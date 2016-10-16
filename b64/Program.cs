6e417bd1f46de697c4fc7275a702d57e85858d6f
SpaceRekt <ownerz0fenemy@hotmail.fr>
2016-10-16 10:45:56 +0200

    Base
    Build 1.0:
    Encode and decode base64 using arguments
    
    Syntax: b64 [e/d] [string]
    e = encode
    d = decode

---
+++ b64/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace b64
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                if(args[0] == "e" || args[0] == "enc" || args[0] == "encode")
+                {
+                    try
+                    {
+                        string res = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Join("", args).Substring(args[0].Length)));
+                        Clipboard.SetText(res);
+                        Console.WriteLine($"Set to clipboard! [{res}]");
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("ERROR: " + e.Message);
+                    }
+                }
+                if(args[0] == "d")
+                {
+                    try
+                    {
+                        string res = Encoding.UTF8.GetString(Convert.FromBase64String(string.Join("", args).Substring(args[0].Length)));
+                        Clipboard.SetText(res);
+                        Console.WriteLine($"Set to clipboard! [{res}]");
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine("ERROR: " + e.Message);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Correct Syntax is: b64 [e/d] [string]");
+            }
+        }
+    }
+}
